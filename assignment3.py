4dca1aeb815af2c7571f6eb84a5433cd5689700e
Weronika Swistowska <143666671+maevemoon@users.noreply.github.com>
2024-05-05 12:09:50 +0100

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -3,13 +3,33 @@
 
 import sys
 import math
-from mygff import *
+import mygff as a2
+import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 import numpy as np
 
-# placeholder
+# there is a problem with this
+# it infinitely asks for an input until you press Exit, in which case the result becomes None
+# while messing around with this sometimes it worked fine sometimes it didn't, i ultimately don't know how to fix it
+# everything works as it should with a placeholder though
 def win_size():
-    return 50
+    sg.theme('GreenMono')
+    layout = [[sg.VPush()], # centering
+              [sg.Push(), sg.Text('Sliding Window for GC Proportion',font=('Franklin Gothic Medium', 12, 'italic', 'bold')), sg.Push()], 
+              [sg.Push(), sg.Text('Enter size of sliding window:'), sg.InputText(key='-IN-'), sg.Push()],
+              [sg.Push(), sg.Button('CONFIRM',key='-confirm-'), sg.Button('EXIT'), sg.Push()], 
+              [sg.VPush()]]
+    window = sg.Window('Sliding Window', layout, size=(300,150))
+    while True:
+        event, values = window.read()
+        print(values.get('-IN-'))
+        if event == '-confirm-':
+            result = values['-IN-']
+            if result:
+                return result
+        elif event in ['Exit', sg.WIN_CLOSED]:
+            break
+    window.close()
 
 # i was unable to get this to work as one function
 # so i did it separately
@@ -17,7 +37,7 @@ def win_size():
 def get_windows(seq):
     for i in range(0, len(seq) - win_size() + 1): # ensuring space for last window
         window = seq[i:i + win_size()]
-        yield window # returning sequence of windows to be then iterated through
+        yield window # returning sequence of windows to be then iterated through to plot
 
 def main():
 
@@ -25,28 +45,31 @@ def main():
 
     with open(seq_file, 'r') as fasta:
 
-        seq = parse_fasta(fasta)
-        gff_details = parse_gff(gff_file)
-        sequences = extract_orfs(gff_details, seq)
+        # setting up
+        seq = a2.parse_fasta(fasta)
+        gff_details = a2.parse_gff(gff_file)
+        sequences = a2.extract_orfs(gff_details, seq)
 
         # secondly, calculate gc proportion per window per gene
-        id = 1 # placing the plots anticlockwise on the subplot grid
+        id = 1 # first graph
         for k, v in sequences.items():
-            x = range(0,len(v) - win_size() + 1)
-            y = []
+            x = range(0,len(v) - win_size() + 1) # nucleotide position
+            y = [] # gc proportion
             for window in get_windows(v):
                 gc_proportion = (window.count("g") + window.count("c")) / len(window)
-                y.append(gc_proportion) # y-axis point to plot on graph
-            ax = plt.subplot(math.ceil(len(sequences)/4),4,id) # ensuring there are enough rows for the plot
+                y.append(gc_proportion)
+            ax = plt.subplot(math.ceil(len(sequences)/4),4,id) # ensuring there are enough rows
             ax.plot(x,y)
-            # decoration, formatting
+
+            # formatting
             ax.set_title(k)
-            ax.axhline(y=np.nanmean(y),color="red") # adding horizontal average line
+            ax.axhline(y=np.nanmean(y),color="red") # gc proportion average line
             ax.set_xlabel("nucleotide position")
             ax.set_ylabel("gc proportion")
-            id +=1
+
+            id +=1 # placing plots anticlockwise
     
-        plt.tight_layout() # so titles etc don't overlap
+        plt.tight_layout() 
         plt.show()
 
 if __name__ == "__main__":
